b856d002c785bdbfa54c3da084a7d2bdfcad8cc1
Chulbuli <53034246+Chulbuli@users.noreply.github.com>
2019-07-18 12:51:11 +0530

    Update MAD_git.java

--- app/src/main/java/lk/sliit/student/myapplication/MAD_git.java
+++ app/src/main/java/lk/sliit/student/myapplication/MAD_git.java
@@ -9,5 +9,6 @@ public class MAD_git extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mad_git);
+        System.out.println("HI !");
     }
 }
